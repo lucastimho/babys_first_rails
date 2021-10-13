db21b0fb25650d3199cd0476af73df76e97c2c4e
Lucas <lucas.t.ho.lh@gmail.com>
2021-10-12 19:20:15 -0500

    Completes wecomes_controller

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,8 @@
+class WelcomesController < ApplicationController
+  def hello_method
+    render json: {message: "Hello from welcomes controller!"}
+  end
+  def about_method
+    render json: {message: "My favorite programming language is ruby!"}
+  end
+end
